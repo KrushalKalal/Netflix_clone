340e8e46addf38c3b99ac45e368ca228ac8b2822
KrushalKalal <krushalkalal@gmail.com>
2022-08-17 01:25:55 +0530

    js added

---
+++ index.js
@@ -0,0 +1,198 @@
+function question_1(){
+    console.log("clicked");
+    var que1 = document.getElementById("question_1");
+    var que2 = document.getElementById("question_2");
+    var que3 = document.getElementById("question_3");
+    var que4 = document.getElementById("question_4");
+    var que5 = document.getElementById("question_5");
+    var que6 = document.getElementById("question_6");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que1.style.display === "none"){
+        que1.style.display = "block";
+        que2.style.display = "none";
+        que3.style.display = "none";
+        que4.style.display = "none";
+        que5.style.display = "none";
+        que6.style.display = "none";
+        text1.innerHTML = "x";
+        text2.innerHTML = "+";
+        text3.innerHTML = "+";
+        text4.innerHTML = "+";
+        text5.innerHTML = "+";
+        text6.innerHTML = "+";
+    }else {
+        que1.style.display = "none";
+        text1.innerHTML = "+"
+      }
+}
+
+function question_2(){
+    console.log("clicked");
+    var que2 = document.getElementById("question_2");
+    var que1 = document.getElementById("question_1");
+    var que3 = document.getElementById("question_3");
+    var que4 = document.getElementById("question_4");
+    var que5 = document.getElementById("question_5");
+    var que6 = document.getElementById("question_6");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que2.style.display === "none"){
+        que2.style.display = "block";
+        que1.style.display = "none";
+        que3.style.display = "none";
+        que4.style.display = "none";
+        que5.style.display = "none";
+        que6.style.display = "none";
+        text2.innerHTML = "x";
+        text1.innerHTML = "+";
+        text3.innerHTML = "+";
+        text4.innerHTML = "+";
+        text5.innerHTML = "+";
+        text6.innerHTML = "+";
+    }else {
+        que2.style.display = "none";
+        text2.innerHTML = "+"
+      }
+}
+
+function question_3(){
+    console.log("clicked");
+    var que3 = document.getElementById("question_3");
+    var que2 = document.getElementById("question_2");
+    var que1 = document.getElementById("question_1");
+    var que4 = document.getElementById("question_4");
+    var que5 = document.getElementById("question_5");
+    var que6 = document.getElementById("question_6");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que3.style.display === "none"){
+        que3.style.display = "block";
+        que2.style.display = "none";
+        que1.style.display = "none";
+        que4.style.display = "none";
+        que5.style.display = "none";
+        que6.style.display = "none";
+        text3.innerHTML = "x";
+        text1.innerHTML = "+";
+        text2.innerHTML = "+";
+        text4.innerHTML = "+";
+        text5.innerHTML = "+";
+        text6.innerHTML = "+";
+    }else {
+        que3.style.display = "none";
+        text3.innerHTML = "+"
+      }
+}
+
+function question_4(){
+    console.log("clicked");
+    var que4 = document.getElementById("question_4");
+    var que2 = document.getElementById("question_2");
+    var que3 = document.getElementById("question_3");
+    var que1 = document.getElementById("question_1");
+    var que5 = document.getElementById("question_5");
+    var que6 = document.getElementById("question_6");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que4.style.display === "none"){
+        que4.style.display = "block";
+        que2.style.display = "none";
+        que3.style.display = "none";
+        que1.style.display = "none";
+        que5.style.display = "none";
+        que6.style.display = "none";
+        text4.innerHTML = "x";
+        text1.innerHTML = "+";
+        text2.innerHTML = "+";
+        text3.innerHTML = "+";
+        text5.innerHTML = "+";
+        text6.innerHTML = "+";
+    }else {
+        que4.style.display = "none";
+        text4.innerHTML = "+"
+      }
+}
+
+function question_5(){
+    console.log("clicked");
+    var que5 = document.getElementById("question_5");
+    var que2 = document.getElementById("question_2");
+    var que3 = document.getElementById("question_3");
+    var que4 = document.getElementById("question_4");
+    var que1 = document.getElementById("question_1");
+    var que6 = document.getElementById("question_6");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que5.style.display === "none"){
+        que5.style.display = "block";
+        que2.style.display = "none";
+        que3.style.display = "none";
+        que4.style.display = "none";
+        que1.style.display = "none";
+        que6.style.display = "none";
+        text5.innerHTML = "x";
+        text1.innerHTML = "+";
+        text2.innerHTML = "+";
+        text3.innerHTML = "+";
+        text4.innerHTML = "+";
+        text6.innerHTML = "+";
+    }else {
+        que5.style.display = "none";
+        text5.innerHTML = "+"
+      }
+}
+
+function question_6(){
+    console.log("clicked");
+    var que6 = document.getElementById("question_6");
+    var que2 = document.getElementById("question_2");
+    var que3 = document.getElementById("question_3");
+    var que4 = document.getElementById("question_4");
+    var que5 = document.getElementById("question_5");
+    var que1 = document.getElementById("question_1");
+    var text1 = document.getElementById("sign1");
+    var text2 = document.getElementById("sign2");
+    var text3 = document.getElementById("sign3");
+    var text4 = document.getElementById("sign4");
+    var text5 = document.getElementById("sign5");
+    var text6 = document.getElementById("sign6");
+    if(que6.style.display === "none"){
+        que6.style.display = "block";
+        que2.style.display = "none";
+        que3.style.display = "none";
+        que4.style.display = "none";
+        que5.style.display = "none";
+        que1.style.display = "none";
+        text6.innerHTML = "x";
+        text5.innerHTML = "x";
+        text1.innerHTML = "+";
+        text2.innerHTML = "+";
+        text3.innerHTML = "+";
+        text4.innerHTML = "+";
+        text5.innerHTML = "+";
+    }else {
+        que6.style.display = "none";
+        text6.innerHTML = "+"
+      }
+}
